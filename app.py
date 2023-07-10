a97553f15b574675c2358448348cd9fc5337e235
heidless <rob.lockhart@yahoo.co.uk>
2023-07-10 18:55:54 +0100

    test

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
 
-
-if __name__ == "__main__":#
-#    app.run(debug=True)
-    app.run(host='0.0.0.0', port=3003, debug=True) # starts the web app at port 8000
+if __name__ == "__main__":
+    app.run(port=3003, debug=True)
+#   app.run(host='0.0.0.0', port=3003, debug=True) # starts the web app at port 3003
